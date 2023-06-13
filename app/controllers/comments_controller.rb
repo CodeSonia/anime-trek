7312a8a529c33182699c440ee3bbd70438f93486
Sonia Hussain <sonia_ahm@icloud.com>
2023-06-13 11:44:59 +0100

    added comments validations and controller methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @episode = Episode.find(params[:episode_id])
+    @user = current_user
+    @comment = @episode.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to episode_path(@comment.episode_id), notice: "Comment was successfully created."
+    else
+      render :new, status: :unprocessable_entity, alert: "Comment was not created."
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id)
+  end
 end
