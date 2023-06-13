1b161b3a34a8582cb6ee4d4e99d7521f8f4e3dbb
Sonia Hussain <sonia_ahm@icloud.com>
2023-06-13 14:31:13 +0100

    Added watch list controller with validations and methods.

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,4 +1,8 @@
 class Watchlist < ApplicationRecord
   belongs_to :user
   belongs_to :anime
+
+  validates :user_id, presence: true
+  validates :anime_id, presence: true
+  validates :status, inlcusion: { in: [true, false] }
 end
