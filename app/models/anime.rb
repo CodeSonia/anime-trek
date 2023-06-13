6acde8218b34873aa450f7ce00bb153a18b758d3
Alexis Sakabenis <alexis.10@outlook.be>
2023-06-13 12:04:51 +0200

    Add Action to Animes controller, routes and validations

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,2 +1,11 @@
 class Anime < ApplicationRecord
+  has_many :seasons
+
+  validates :title, presence: true
+  validates :synopsis, presence: true
+  validates :date_start, presence: true
+  validates :date_finish, presence: true
+  validates :genre, presence: true
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :image, presence: true
 end
