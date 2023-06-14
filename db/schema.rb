ade8ac3becfb94189b01832c00e0d4c5fd498604
Sonia Hussain <sonia_ahm@icloud.com>
2023-06-14 15:23:03 +0100

    Updated animes schema to take up new fields, rank, image array and embed_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_134328) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_141926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_134328) do
     t.datetime "updated_at", null: false
     t.integer "episodecount"
     t.integer "api_id"
+    t.integer "rank"
+    t.text "image_urls", default: [], array: true
+    t.string "embed_url"
   end
 
   create_table "comments", force: :cascade do |t|
